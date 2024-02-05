1b6578842d76f48492675c17411c2c00df28ed88
CJC0512 <ehdchs2006@gmail.com>
2024-02-05 10:40:21 +0900

    chap02 생성 및 JDBCTemplate, connection-info.properties 작성

---
+++ chap02-statements-lecture-source/src/main/java/com/ohgiraffers/common/JDBCTemplate.java
@@ -0,0 +1,44 @@
+package com.ohgiraffers.common;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JDBCTemplate {
+    public static Connection getConnection(){
+        Connection con = null;
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileReader("src/main/java/com/ohgiraffers/config/connection-info.properties"));
+
+            String driver = prop.getProperty("driver");
+            String url = prop.getProperty("url");
+
+            Class.forName(driver);
+
+            con = DriverManager.getConnection(url, prop);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return con;
+    }
+
+    public static void close(Connection con){
+        try {
+            if (con != null && !con.isClosed()) con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
